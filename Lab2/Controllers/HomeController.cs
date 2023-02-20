57fefba71b4ac66e906adae71fe5f688a98cf0a7
chris <christriwu@gmail.com>
2023-02-20 12:56:36 -0500

    Complete Lab2

---
+++ Lab2/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Lab2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Lab2.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Fifteen = 0;
+            ViewBag.Twenty = 0;
+            ViewBag.TwentyFive = 0;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(TipCalculator calc)
+        {
+            if (ModelState.IsValid)
+            {
+               ViewBag.Fifteen =  calc.CalculateTip(0.15);
+               ViewBag.Twenty =  calc.CalculateTip(0.20);
+               ViewBag.TwentyFive= calc.CalculateTip(0.25);
+            }
+            else
+            {
+                ViewBag.Fifteen = 0;
+                ViewBag.Twenty = 0;
+                ViewBag.TwentyFive = 0;
+            }
+
+
+            return View(calc);
+        }
+
+    }
+}
---
+++ Lab2/Models/TipCalculator.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab2.Models
+{
+    public class TipCalculator
+    {
+        [Required(ErrorMessage = "Please enter a meal cost")]
+        [Range(0,10000, ErrorMessage = "Cost of the meal should be greater than $0 and less than $10000")]
+        public double? MealCost { get; set; }
+
+        public double CalculateTip(double percent)
+        {
+            if (MealCost.HasValue)
+            {
+                var tip = MealCost.Value * percent;
+                return tip;
+            }
+            return 0.0;
+        }
+
+        
+    }
+
+
+}
+
